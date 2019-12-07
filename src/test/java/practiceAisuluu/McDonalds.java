00fa0a7d61daa898941a5c712b4960d83db7a3a7
Aisuluu Darmanova <aisuluu1516@gmail.com>
2019-12-06 22:08:53 -0600

    some notes added

--- src/test/java/practiceAisuluu/McDonalds.java
+++ src/test/java/practiceAisuluu/McDonalds.java
@@ -4,5 +4,6 @@ public class McDonalds {
 
     public static void main(String[] args) {
         System.out.println("Aisuluu is working on this class");
+        System.out.println("I am taking notes");
     }
 }
