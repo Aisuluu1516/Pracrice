c01f1f02bac8612234a5991c96a7fea83b2e2fcf
Aisuluu Darmanova <aisuluu1516@gmail.com>
2019-12-06 23:27:21 -0600

    Mc class: Cholpon started working

--- src/test/java/practiceAisuluu/McDonalds.java
+++ src/test/java/practiceAisuluu/McDonalds.java
@@ -5,5 +5,6 @@ public class McDonalds {
     public static void main(String[] args) {
         System.out.println("Aisuluu is working on this class");
         System.out.println("I am taking notes");
+        System.out.println("Cholpon started working on this class");
     }
 }
