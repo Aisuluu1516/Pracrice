056689a2788ca75e563c9240e2fda2b30590e5f5
Aisuluu Darmanova <aisuluu1516@gmail.com>
2019-12-17 14:45:09 -0600

    first change with Tugbe

--- src/test/java/practiceAisuluu/McDonalds.java
+++ src/test/java/practiceAisuluu/McDonalds.java
@@ -5,6 +5,7 @@ public class McDonalds {
     public static void main(String[] args) {
         System.out.println("Aisuluu is working on this class");
         System.out.println("I am taking notes");
+        System.out.println(" Making change with Tugba");
 
     }
 }
